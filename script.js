897aba43f30b2ee1bfa953b81482f97b776af2df
Jordan Long <jblong27@gmail.com>
2020-05-28 17:47:18 -0500

    pushing my current progress. Almost finished building the html for the quiz

---
+++ script.js
@@ -0,0 +1,44 @@
+// variables here
+var startEl = document.getElementById("start");
+var 
+// variable / object to contain my quiz questions
+var quizQuestions = [
+{
+    question: "Inside which HTML element do we put the JavaScript?",
+    answerA: "<script>",
+    answerB: "<javascript>",
+    answerC: "<js>",
+    answerD: "<scripting>",
+    rightAnswer: "a"},
+{
+    question: "How do you begin a loop?",
+    answerA: "if",
+    answerB: "else",
+    answerC: "else if",
+    answerD: "for",
+    rightAnswer: "d"},
+{
+    question: "Which event occurs when a user clicks on an HTML element?",
+    answerA: "onchange",
+    answerB: "onclick",
+    answerC: "onmouseclick",
+    answerD: "onmouseover",
+    rightAnswer: "b"},
+{
+    question: "How do you declare a JavaScript variable?",
+    answerA: "variable carName",
+    answerB: "var carName",
+    answerC: "v carName",
+    answerD: "declare carName"},
+];
+
+// function to run through the quiz questions and answers
+
+// function to start timer
+   // need to take time off the clock for an incorrect answer
+
+// 
+
+//
+
+// function to store the scores
